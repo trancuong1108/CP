41e514533dd2291757a4a3145edc85f709c18371
trancuong1108 <81424101+trancuong1108@users.noreply.github.com>
2023-10-03 14:58:49 +0700

    Create fastscanVIP.cpp

---
+++ fastscanVIP.cpp
@@ -0,0 +1,35 @@
+namespace IO {
+    const int BUFSIZE = 1<<14;
+    char buf[BUFSIZE + 1], *inp = buf;
+
+    bool reacheof;
+    char get_char() {
+        if (!*inp && !reacheof) {
+            memset(buf, 0, sizeof buf);
+            int tmp = fread(buf, 1, BUFSIZE, stdin);
+            if (tmp != BUFSIZE) reacheof = true;
+            inp = buf;
+        }
+        return *inp++;
+    }
+    template<typename T>
+    T get() {
+        int neg = 0;
+        T res = 0;
+        char c = get_char();
+        while (!(c >= '0' && c <= '9') && c != '-' && c != '+') c = get_char();
+        if (c == '+') { neg = 0; }
+        else if (c == '-') { neg = 1; }
+        else res = c - '0';
+
+        c = get_char();
+        while (c >= '0' && c <= '9') {
+            res = res * 10 + (c - '0');
+            c = get_char();
+        }
+        return neg ? -res : res;
+    }
+};
+int read() {
+    return IO::get<int>();
+}
